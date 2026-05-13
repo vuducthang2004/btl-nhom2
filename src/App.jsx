9ada0d2cc33706ba5d5bef70035fb438f16ce799
Ten cua ban <email@example.com>
2026-05-13 20:56:41 +0700

    sửa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import DangNhap from './dangnhap';
-import TrangChu from './TrangChu';
-import QuanLyNhanSu from './QuanLyNhanSu';
-import SoDoBan from './SoDoBan';
+import TrangChu from './trangchu';
+import QuanLyNhanSu from './quanlynhansu';
+import SoDoBan from './sodoban';
+
+import OrderReception from './OrderReception'; 
+import InventoryManager from './InventoryManager';
+import MenuManager from './MenuManager';
+/*import ReportDashboard from './ReportDashboard';*/
+import AdminDashboard from './AdminDashboard'; 
 
 function App() {
     return (
@@ -13,6 +20,11 @@ function App() {
                 <Route path="/home" element={<TrangChu />} />
                 <Route path="/admin" element={<QuanLyNhanSu />} />
                 <Route path="/tables" element={<SoDoBan />} />
+
+                <Route path="/phache" element={<OrderReception />} />
+                <Route path="/kho" element={<InventoryManager />} />
+                <Route path="/menu" element={<MenuManager />} />
+                <Route path="/admindashboard" element={<AdminDashboard />} />
             </Routes>
         </Router>
     );
